8711f6bf693480bff623c5c9edfb6a1089002721
RoiOrt2021 <79807089+RoiOrt2021@users.noreply.github.com>
2021-03-01 13:11:04 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,10 @@ const {UserModel ,validUser , validLogin, genToken} = require("../models/userMod
 const { authToken } = require('../middlewares/auth');
 const router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', async(req, res) => {
   try{
-  // let data = await UserModel.find({},{email:1,name:1})
-  // הפרמטר השני בעצם פילטר איזה מאפיינים להציג
-  //  0 -> רק לא להציג אותו
-  // 1 -> רק להציג אותו
+ 
   let data = await UserModel.find({},{pass:0})
   res.json(data);
   }
@@ -23,7 +20,7 @@ router.get('/', async(req, res) => {
 
 router.get("/myInfo",authToken ,async(req,res) => {
   try{
-    // req.userData -> נוצר בקובץ מידלווארי אוט
+    
     let user = await UserModel.findOne({_id:req.userData._id},{pass:0});
     res.json(user);
   }
@@ -40,7 +37,7 @@ router.post("/login",async(req,res) => {
     return res.status(400).json(validBody.error.details);
   }
   try{
-    // קודם בודק שבכלל קיים יוזר עם האימייל הנל
+    
     let user = await UserModel.findOne({email:req.body.email});
     if(!user){
       return res.status(400).json({msg:"user or password invalid 1"});
@@ -59,7 +56,7 @@ router.post("/login",async(req,res) => {
 })
 
 
-// Sign up new user
+
 router.post("/", async(req,res) => {
   let validBody = validUser(req.body);
   if (validBody.error) {
@@ -67,12 +64,12 @@ router.post("/", async(req,res) => {
   }
   try{
     let user = new UserModel(req.body);
-    // אנחנו נרצה דבר ראשון להגדיר את רמת האבטחה
+   
     let salt = await bcrypt.genSalt(10);
-    // זה להגדיר שהסיסמא תיהיה מוצפנת לפי רמת האבטחה שקבענו
+    
     user.pass = await bcrypt.hash(user.pass, salt);
     await user.save();
-    // TODO: show to client just the email, _id, createedAt, name
+   
     res.status(201).json(_.pick(user,["_id","email","date_created","name"]))
   }
   catch (err) {
